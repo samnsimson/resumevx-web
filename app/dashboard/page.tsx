ca900de6739126103379b1b8beede35d40e00a87
samnsimson <samnishanth01@gmail.com>
2026-01-06 17:22:40 -0500

    parse headers before request

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,10 +3,11 @@ import { headers } from 'next/headers';
 import { SessionStateApi } from '@/lib/api';
 import { ChatAndPreviewComponent } from '@/components/dashboard/chat-and-preview';
 import { StarterComponent } from '@/components/dashboard/starter';
+import { parseHeaders } from '@/lib/utils';
 
 export default async function DashboardPage() {
 	const nextHeaders = await headers();
-	const { data: sessionState } = await SessionStateApi.getSessionState({ headers: nextHeaders });
+	const { data: sessionState } = await SessionStateApi.getSessionState({ headers: parseHeaders(nextHeaders) });
 
 	return <Stack boxSize={'full'}>{sessionState ? <ChatAndPreviewComponent sessionState={sessionState} /> : <StarterComponent />}</Stack>;
 }
